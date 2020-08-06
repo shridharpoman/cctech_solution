d78e617cad336a328912839226835c232b28a879
shridharpoman <47786799+shridharpoman@users.noreply.github.com>
2020-08-07 03:10:16 +0530

    first

---
+++ api/app.py
@@ -0,0 +1,135 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import numpy as np
+import ast
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/check',methods=['GET', 'POST'])
+def hello():
+    data = request.json
+    print(data)
+    if data['polygon']:
+
+        data=data['polygon']
+        polyarr = data['polygonArray']
+        # polyarr =np.array(polyarr)
+        # print(type(polyarr))
+        point = data['point']
+        point = point.strip('][').split(',') 
+        x = int(point[0])
+        y = int(point[1])
+        # print(polyarr)
+        isInside = point_inside_polygon(x,y,polyarr)
+        if isInside==True:
+            isInside = "True"
+        else:
+            isInside = "False"    
+    return jsonify(isInside)    
+
+
+    # print(isInside)
+    
+
+@app.route('/sun',methods=['GET', 'POST'])   
+def sun():
+    data = request.json
+    if data['sun']:
+        data=data['sun']
+        buildarr = data['Buildings']
+        buildarr =eval(buildarr)
+        point = data['point']
+        point = point.strip('][').split(',')
+        # print(buildarr) 
+        n = len(buildarr)
+        if n ==1:
+            output=exposedToSunSingle(buildarr,point)
+        else:    
+            near=nearestBuilding(buildarr)
+            output=exposedToSunMultiple(buildarr,point,near) 
+    return jsonify(output)    
+
+def point_inside_polygon(x,y,poly,include_edges=True):
+    poly=eval(poly)
+    n = len(poly)
+    inside = False
+
+    p1x, p1y = poly[0]
+    for i in range(1, n + 1):
+        p2x, p2y = poly[i % n]
+        if p1y == p2y:
+            if y == p1y:
+                if min(p1x, p2x) <= x <= max(p1x, p2x):
+                    # point is on horisontal edge
+                    inside = include_edges
+                    break
+                elif x < min(p1x, p2x):  # point is to the left from current edge
+                    inside = not inside
+        else:  # p1y!= p2y
+            if min(p1y, p2y) <= y <= max(p1y, p2y):
+                xinters = (y - p1y) * (p2x - p1x) / float(p2y - p1y) + p1x
+
+                if x == xinters:  # point is right on the edge
+                    inside = include_edges
+                    break
+
+                if x < xinters:  # point is to the left from current edge
+                    inside = not inside
+
+        p1x, p1y = p2x, p2y
+
+    return inside
+
+
+def exposedToSunMultiple(arr,sun,near):
+    # inde =arr.index(near)
+    index_row = [arr.index(row) for row in arr if near in row][0]
+    first_build = arr[index_row]
+    if index_row == 0:
+        sec_build = arr[1]
+    sec_build = arr[0]    
+    exposed = abs(first_build[1][1] - first_build[0][1])
+    exposed += abs(first_build[3][0]- first_build[0][0])
+    tanTheta = findTheta(first_build,sun)
+    diffBuild = sec_build[0][0] - first_build[3][0]
+    extra_len = tanTheta*diffBuild
+    exposed += extra_len + abs(first_build[0][1])
+    exposed += sec_build[3][0] - sec_build[0][0]
+    exposed = float(exposed)
+    return exposed
+
+def exposedToSunSingle(arr,sun):
+    # print(len(arr))
+    first_build = arr[0]
+    
+    exposed = abs(first_build[1][1] - first_build[0][1])
+    exposed += abs(first_build[3][0]- first_build[0][0])
+    exposed = float(exposed)
+    
+    return exposed
+
+
+def findTheta(first_build,sun):
+    side1 = float(sun[1]) + float(abs(first_build[0][1]))
+    width1 = abs(float(sun[0])) + first_build[3][0]
+    # print(width1)
+    tanTheta = side1/width1
+    # print(tanTheta)
+
+    return tanTheta
+
+def nearestBuilding(arr):
+    near_list = []
+    xy = [0,0]
+    for i in arr:
+        dist = lambda x, y: (x[0]-y[0])**2 + (x[1]-y[1])**2
+        a = min(i, key=lambda co: dist(co, xy))
+        # print(a)
+        near_list.append(a)
+    dist = lambda x, y: (x[0]-y[0])**2 + (x[1]-y[1])**2
+    a = min(near_list, key=lambda co: dist(co, xy))
+    return a   
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
---
+++ src/App.js
@@ -0,0 +1,225 @@
+import React from "react";
+
+import "./App.css";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import axios from "axios";
+// import axios from "axios";
+
+const useStyles = (theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  textinput: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+  pos: {
+    width: "25ch",
+  },
+  cardd: {
+    display: "flex",
+    justifyContent: "center",
+    "margin-left": "5%",
+    width: "90%",
+  },
+  title: {
+    fontSize: 44,
+  },
+});
+
+class App extends React.Component {
+  state = {
+    showFieldPoly: false,
+    showFieldSun: false,
+    selectedValue: "",
+    message: "",
+    polygonArray: "",
+    point: "",
+    polyanswer: "",
+    sunAnswer: "",
+  };
+
+  handleFirstFieldChange = (event) => {
+    const { value } = event.target;
+    this.setState({ polygonArray: value });
+  };
+
+  handleSecondFieldChange = (event) => {
+    const { value } = event.target;
+    this.setState({ point: value });
+  };
+
+  checkLayers = (event) => {
+    const { value } = event.target;
+
+    this.setState({
+      showFieldPoly: value === "Poly",
+      showFieldSun: value === "Sun",
+      selectedValue: value,
+      message: "",
+    });
+  };
+
+  dataSubmit = (event) => {
+    console.log(this.state.polygonArray, this.state.point);
+  };
+
+  handleSubmitPolygon = (event) => {
+    event.preventDefault();
+    const polygon = {
+      polygonArray: this.state.polygonArray,
+      point: this.state.point,
+    };
+
+    axios.post(`http://localhost:8000/check`, { polygon }).then((res) => {
+      console.log(res.data);
+      this.setState({ polyanswer: res.data });
+    });
+  };
+
+  handleSubmitSun = (event) => {
+    event.preventDefault();
+    const sun = {
+      Buildings: this.state.polygonArray,
+      point: this.state.point,
+    };
+
+    axios.post(`http://localhost:8000/sun`, { sun }).then((res) => {
+      console.log(res.data);
+      this.setState({ sunAnswer: res.data });
+    });
+  };
+
+  render() {
+    const { selectedValue, showFieldPoly, showFieldSun } = this.state;
+    const { classes } = this.props;
+
+    return (
+      <div className="App">
+        <FormControl variant="outlined" className={classes.formControl}>
+          <InputLabel id="demo-simple-select-outlined-label">
+            Problem
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-outlined-label"
+            id="demo-simple-select-outlined"
+            value={selectedValue}
+            onChange={this.checkLayers}
+            label="Problem"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value="Poly">Polygon Problem</MenuItem>
+            <MenuItem value="Sun">Sun Covering Problem</MenuItem>
+          </Select>
+        </FormControl>
+        <form className={classes.textinput}>
+          {showFieldPoly ? (
+            <TextField
+              id="Array"
+              label="Array of Polygon"
+              variant="outlined"
+              onChange={this.handleFirstFieldChange}
+              value={this.state.polygonArray}
+            />
+          ) : null}
+          {showFieldPoly ? (
+            <>
+              <TextField
+                id="Point"
+                label="Point to find out"
+                variant="outlined"
+                onChange={this.handleSecondFieldChange}
+                value={this.state.point}
+              />
+              <Button
+                className={classes.pos}
+                variant="contained"
+                onClick={this.handleSubmitPolygon}
+              >
+                Default
+              </Button>
+              <div>
+                <Card className={classes.cardd}>
+                  <CardContent>
+                    <Typography
+                      className={classes.title}
+                      variant="h5"
+                      component="h2"
+                    >
+                      {this.state.polyanswer}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </div>
+            </>
+          ) : null}
+          {showFieldSun ? (
+            <TextField
+              id="Array"
+              label="Array of Buildings"
+              variant="outlined"
+              onChange={this.handleFirstFieldChange}
+              value={this.state.polygonArray}
+            />
+          ) : null}
+          {showFieldSun ? (
+            <>
+              <TextField
+                id="Sun"
+                label="Coodinate of Sun"
+                variant="outlined"
+                onChange={this.handleSecondFieldChange}
+                value={this.state.point}
+              />
+              <Button
+                className={classes.pos}
+                variant="contained"
+                onClick={this.handleSubmitSun}
+              >
+                Default
+              </Button>
+              <div>
+                <Card className={classes.cardd}>
+                  <CardContent>
+                    <Typography
+                      className={classes.title}
+                      variant="h5"
+                      component="h2"
+                    >
+                      {this.state.sunAnswer}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </div>
+            </>
+          ) : null}
+
+          {/* <div>
+            <h1>{this.state.polyanswer}</h1>
+          </div> */}
+        </form>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(useStyles)(App);
